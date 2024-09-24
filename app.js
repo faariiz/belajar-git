c5449ee5666927db3b729a82e17572f04600f1a7
faariiz <faariiz.abdurrasyid@gmail.com>
2024-09-24 20:23:10 +0700

    initialize project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express'); 
+const app = express();
+const PORT = 8000;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+})
+
+module.exports = app
